b7e2b8e928618b9d35f4741ba6d08e68e44584f1
alubin <andylubin@hotmail..com>
2016-05-19 11:06:16 -0500

    Added Database Library

--- Web/src/status/V1Status.java
+++ Web/src/status/V1Status.java
@@ -9,12 +9,21 @@ import javax.ws.rs.core.MediaType;
 public class V1Status {
 	
 	private static int counter=1000;
+	private static final String version = "00.01.00";
 
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String returnTitle()
 	{
 		return "<p>Java Web Service</p>" + counter++;
+	}
+	
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion()
+	{
+		return "<p>Version:</p>" + version;
 	}
 
 }
